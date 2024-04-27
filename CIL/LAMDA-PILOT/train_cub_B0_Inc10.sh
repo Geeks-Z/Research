9f76d0b73d3868eecf204cace9223607a593dc13
Geeks_Z <izhw1024@163.com>
2024-04-27 22:26:10 +0800

    新增baseline设置

--- CIL/LAMDA-PILOT/train_cub_B0_Inc10.sh
+++ CIL/LAMDA-PILOT/train_cub_B0_Inc10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python main.py --config=./exps/finetune_cub_B0.json
+python main.py --config=./exps/finetune_cub_B0_Inc5.json
 python main.py --config=./exps/icarl_cub_B0.json
 python main.py --config=./exps/der_cub_B0.json
 python main.py --config=./exps/foster_cub_B0.json
--- CIL/LAMDA-PILOT/train_omn_dual.sh
+++ CIL/LAMDA-PILOT/train_omn_dual.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#python main.py --config=./exps/finetune_omn_B0.json
+#python main.py --config=./exps/finetune_omn_B0_Inc30.json
 #python main.py --config=./exps/simplecil_omn_B0.json
 #python main.py --config=./exps/l2p_omn_B0.json
 python main.py --config=./exps/dualprompt_omn_B0.json
