c38c90da8f42dcf123706431d4c8ce9bc4386009
Geeks_Z <izhw1024@163.com>
2024-04-27 22:26:27 +0800

    新增baseline设置

---
+++ CIL/LAMDA-PILOT/train_omn_B0_Inc30.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python main.py --config=./exps/finetune_omn_B0_Inc30.json
+python main.py --config=./exps/icarl_omn_B0_Inc30.json
+python main.py --config=./exps/der_omn_B0_Inc30.json
+python main.py --config=./exps/foster_omn_B0_Inc30.json
+python main.py --config=./exps/l2p_omn_B0_Inc30.json
+python main.py --config=./exps/dualprompt_omn_B0_Inc30.json
+python main.py --config=./exps/coda_prompt_omn_B0_Inc30.json
+python main.py --config=./exps/simplecil_omn_B0_Inc30.json
+python main.py --config=./exps/ease_omn_B0_Inc30.json
