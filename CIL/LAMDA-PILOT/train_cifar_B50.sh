9d35471e9d6562a76e962e453ced8b384d45cd98
Geeks_Z <izhw1024@163.com>
2024-04-29 08:59:09 +0800

    baseline更新

--- CIL/LAMDA-PILOT/train_cifar_B50.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#python main.py --config=./exps/finetune_B50.json
-#python main.py --config=./exps/simplecil_B50.json
-#python main.py --config=./exps/der_B50.json
-python main.py --config=./exps/foster_B50.json
-python main.py --config=./exps/l2p_B50.json
-python main.py --config=./exps/dualprompt_B50.json
-python main.py --config=./exps/coda_prompt_B50.json
-python main.py --config=./exps/ease_B50.json
--- CIL/LAMDA-PILOT/train_icarl_B50.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python main.py --config=./exps/icarl_cifar_B50_Inc5.json
-python main.py --config=./exps/icarl_cub_B100_Inc5.json
-python main.py --config=./exps/icarl_inr_B100_Inc5.json
-python main.py --config=./exps/icarl_omn_B150_Inc5.json
-
-
-
-
