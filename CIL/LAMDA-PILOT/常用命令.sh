de5ad017899329842d621ae03b6e03d04a4bf7a7
Geeks_Z <izhw1024@163.com>
2024-04-30 21:02:05 +0800

    新增baseline设置

--- CIL/LAMDA-PILOT/常用命令.sh
+++ CIL/LAMDA-PILOT/常用命令.sh
@@ -2,4 +2,6 @@ cd Code/Research/CIL/LAMDA-PILOT/
 
 nohup ./train_omn_B0_Inc30.sh > ./res/supp-baseline-omn-B0-Inc30-ease.out 2>&1 &
 
+nohup ./train_omn_B150_Inc5.sh > ./res/2nd-baseline-omn-B150-Inc5.out 2>&1 &
+
 nohup ./train_cub_B100_Inc5.sh > ./res/2nd-baseline-cub-B100-Inc5.out 2>&1 &
