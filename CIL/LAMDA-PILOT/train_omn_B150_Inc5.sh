8ae0a2103c11bc164b8de993156a35c79b17d599
Geeks_Z <izhw1024@163.com>
2024-05-07 08:10:51 +0800

    baseline 配置文件统一命名

--- CIL/LAMDA-PILOT/train_omn_B150_Inc5.sh
+++ CIL/LAMDA-PILOT/train_omn_B150_Inc5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python main.py --config=./exps/finetune_omn_B150_Inc5.json
 python main.py --config=./exps/icarl_omn_B150_Inc5.json
-python main.py --config=./exps/der_omn_B150_Inc5.json
-python main.py --config=./exps/foster_omn_B150_Inc5.json
-python main.py --config=./exps/l2p_omn_B150_Inc5.json
-python main.py --config=./exps/dualprompt_omn_B150_Inc5.json
-python main.py --config=./exps/coda_prompt_omn_B150_Inc5.json
-python main.py --config=./exps/simplecil_omn_B150_Inc5.json
-python main.py --config=./exps/ease_omn_B150_Inc5.json
+#python main.py --config=./exps/der_omn_B150_Inc5.json
+#python main.py --config=./exps/foster_omn_B150_Inc5.json
+#python main.py --config=./exps/l2p_omn_B150_Inc5.json
+#python main.py --config=./exps/dualprompt_omn_B150_Inc5.json
+#python main.py --config=./exps/coda_prompt_omn_B150_Inc5.json
+#python main.py --config=./exps/simplecil_omn_B150_Inc5.json
+#python main.py --config=./exps/ease_omn_B150_Inc5.json
 
--- CIL/LAMDA-PILOT/常用命令.sh
+++ CIL/LAMDA-PILOT/常用命令.sh
@@ -5,3 +5,7 @@ nohup ./train_omn_B0_Inc30.sh > ./res/supp-baseline-omn-B0-Inc30-ease.out 2>&1 &
 nohup ./train_omn_B150_Inc5.sh > ./res/2nd-baseline-omn-B150-Inc5.out 2>&1 &
 
 nohup ./train_cub_B100_Inc5.sh > ./res/2nd-baseline-cub-B100-Inc5.out 2>&1 &
+
+nohup ./train_omn_B0_Inc30.sh > ./res/3nd-baseline-omn-B0-Inc30.out 2>&1 &
+
+nohup ./train_cifar_B0_Inc5.sh > ./res/3nd-baseline-cifar-B0-Inc5.out 2>&1 &
